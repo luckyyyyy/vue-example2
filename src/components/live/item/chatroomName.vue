e244ac6a16b51afb5512271a3c1dc0e7b8074d26
William Chan <root@williamchan.me>
2017-03-10 03:10:54 +0800

    。。。

--- src/components/live/item/chatroomName.vue
+++ src/components/live/item/chatroomName.vue
@@ -1,6 +1,12 @@
 <template>
 	<el-dropdown
-		v-if="!members[account] || members[account] && members[account].type != 'owner'"
+		v-if="
+			!members[account] ||
+			members[account] &&
+			members[account].type != 'owner' &&
+			account != user.accid &&
+			getAuth(2)
+			"
 		trigger="click"
 		style="cursor: pointer"
 	>
@@ -8,28 +14,28 @@
 		<el-dropdown-menu slot="dropdown">
 			<el-dropdown-item disabled>{{ nickname }}</el-dropdown-item>
 			<template v-if="!members[account]">
-				<el-dropdown-item @click.native="setUser('manager', true)">设为管理员</el-dropdown-item>
-				<el-dropdown-item @click.native="setUser('common', true)">设为会员</el-dropdown-item>
-				<el-dropdown-item @click.native="setUser('gag', true)" divided>禁言</el-dropdown-item>
+				<el-dropdown-item v-show="getAuth(1)" @click.native="setUser('manager', true)">设为管理员</el-dropdown-item>
+				<el-dropdown-item v-show="getAuth(2)" @click.native="setUser('common', true)">设为会员</el-dropdown-item>
+				<el-dropdown-item v-show="getAuth(2)" @click.native="setUser('gag', true)" divided>禁言</el-dropdown-item>
 			</template>
 			<template v-else>
 				<template v-if="members[account].type != 'manager'">
-					<el-dropdown-item @click.native="setUser('manager', true)">设为管理员</el-dropdown-item>
+					<el-dropdown-item v-show="getAuth(1)" @click.native="setUser('manager', true)">设为管理员</el-dropdown-item>
 				</template>
 				<template v-else-if="members[account].type == 'manager'">
-					<el-dropdown-item @click.native="setUser('manager', false)">取消管理员</el-dropdown-item>
+					<el-dropdown-item v-show="getAuth(1)" @click.native="setUser('manager', false)">取消管理员</el-dropdown-item>
 				</template>
 				<template v-if="members[account].type != 'manager' && members[account].type != 'common'">
-					<el-dropdown-item @click.native="setUser('common', true)">设为会员</el-dropdown-item>
+					<el-dropdown-item v-show="getAuth(2)" @click.native="setUser('common', true)">设为会员</el-dropdown-item>
 				</template>
 				<template v-else-if="members[account].type == 'common'">
-					<el-dropdown-item @click.native="setUser('common', false)">取消会员</el-dropdown-item>
+					<el-dropdown-item v-show="getAuth(2)" @click.native="setUser('common', false)">取消会员</el-dropdown-item>
 				</template>
 				<template v-if="members[account].type != 'restricted'">
-					<el-dropdown-item @click.native="setUser('gag', true)" divided>禁言</el-dropdown-item>
+					<el-dropdown-item v-show="getAuth(2)" @click.native="setUser('gag', true)" divided>禁言</el-dropdown-item>
 				</template>
 				<template v-else-if="members[account].type == 'restricted'">
-					<el-dropdown-item @click.native="setUser('gag', false)" divided>解除禁言</el-dropdown-item>
+					<el-dropdown-item v-show="getAuth(2)" @click.native="setUser('gag', false)" divided>解除禁言</el-dropdown-item>
 				</template>
 			</template>
 		</el-dropdown-menu>
@@ -47,7 +53,26 @@
 			members: Object,
 			me: Boolean,
 		},
+		computed: {
+			user () {
+				return this.$store.state.user.userImInfo;
+			}
+		},
 		methods: {
+			getAuth (level) {
+				const member = this.members[this.user.accid];
+				if (member) {
+					switch (level) {
+						case 1:
+							return ['owner'].includes(member.type);
+						case 2:
+							return ['owner', 'manager'].includes(member.type);
+						default:
+							// statements_def
+							break;
+					}
+				}
+			},
 			setUser (command, isAdd) {
 				const account = this.account;
 				switch (command) {
