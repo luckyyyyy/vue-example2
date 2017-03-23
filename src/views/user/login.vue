b727135307c816b717f050296d42c930c5f967c5
William Chan <root@williamchan.me>
2017-03-23 18:11:45 +0800

    fixed

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,30 +1,34 @@
 <template>
-	<div class="form">
-		<div class="logo">
-			<img src="../../assets/toplogo.png" height="40" width="196">
-		</div>
-		<div class="title">Sign in to Rainbow Live</div>
-		<iForm
-			:model="login"
-			@submit.native.prevent
-			:rules="rules"
-			:label-width="75"
-			ref="login"
-		>
-			<FormItem label="账号登录" prop="phone">
-				<iInput v-model="login.phone" placeholder="请输手机号码"></iInput>
-			</FormItem>
-			<FormItem label="登录密码" prop="password">
-				<iInput type="password" v-model="login.password" placeholder="请输入您的账户密码"></iInput>
-			</FormItem>
-			<div class="button">
-				<iButton :loading="lock" size="large" htmlType="submit" type="primary" @click="submit_login">登录</iButton>
+	<div>
+		<div class="view">
+			<iForm
+				:model="login"
+				@submit.native.prevent
+				:rules="rules"
+				:label-width="75"
+				ref="login"
+				class="form"
+			>
+				<h1 class="title">登录／Login</h1>
+				<FormItem label="账号登录" prop="phone">
+					<iInput v-model="login.phone" placeholder="请输手机号码"></iInput>
+				</FormItem>
+				<FormItem label="登录密码" prop="password">
+					<iInput type="password" v-model="login.password" placeholder="请输入您的账户密码"></iInput>
+				</FormItem>
+				<div class="button">
+					<iButton :loading="lock" size="large" htmlType="submit" type="primary" @click="submit_login">登录</iButton>
+				</div>
+			<div class="line">
+				<router-link class="resetpwd" :to="{ name: 'resetpwd' }">找回密码</router-link>
 			</div>
-		</iForm>
-		<div class="line">
-			<router-link class="resetpwd" :to="{ name: 'resetpwd' }">忘记彩虹云直播管理后台密码？</router-link>
-			<router-link class="register" :to="{ name: 'register' }">没有账号，立即注册？</router-link>
+
+			</iForm>
 		</div>
+		<ul class="tab">
+			<li><span>登录</span></li>
+			<li><router-link class="register" :to="{ name: 'register' }">注册</router-link></li>
+		</ul>
 	</div>
 </template>
 
--- src/views/user/reset_password.vue
+++ src/views/user/reset_password.vue
@@ -1,31 +1,35 @@
 <template>
-	<div class="form">
-		<div class="title">找回密码</div>
-		<iForm
-			:model="reset_password"
-			@submit.native.prevent
-			:rules="rules"
-			:label-width="85"
-			ref="reset_password"
-		>
-			<FormItem label="手机号码" prop="phone">
-				<iInput v-model="reset_password.phone" placeholder="请输手机号码"></iInput>
-			</FormItem>
-			<FormItem label="短信验证码" prop="captcha">
-				<iInput v-model="reset_password.captcha" placeholder="请输入短信验证码">
-					<iButton slot="append" :disabled="lock_captcha" @click="reset_password_captcha">{{ countdown ? countdown : '获取验证码' }}</iButton>
-				</iInput>
-			</FormItem>
-			<FormItem label="设置密码" prop="password">
-				<iInput type="password" v-model="reset_password.password" placeholder="密码"></iInput>
-			</FormItem>
-			<div class="button">
-				<iButton :loading="lock" htmlType="submit" type="primary" @click="submit_reset_password">确认修改</iButton>
-			</div>
-		</iForm>
-		<div class="line">
-			<router-link :to="{ name: 'login' }">已有账号，立即登录！</router-link>
+	<div>
+		<div class="view">
+			<iForm
+				:model="reset_password"
+				@submit.native.prevent
+				:rules="rules"
+				:label-width="85"
+				ref="reset_password"
+				class="form"
+			>
+				<h1 class="title">找回密码／Find Your Password</h1>
+				<FormItem label="手机号码" prop="phone">
+					<iInput v-model="reset_password.phone" placeholder="请输手机号码"></iInput>
+				</FormItem>
+				<FormItem label="短信验证码" prop="captcha">
+					<iInput v-model="reset_password.captcha" placeholder="请输入短信验证码">
+						<iButton slot="append" :disabled="lock_captcha" @click="reset_password_captcha">{{ countdown ? countdown : '获取验证码' }}</iButton>
+					</iInput>
+				</FormItem>
+				<FormItem label="设置密码" prop="password">
+					<iInput type="password" v-model="reset_password.password" placeholder="密码"></iInput>
+				</FormItem>
+				<div class="button">
+					<iButton :loading="lock" htmlType="submit" type="primary" @click="submit_reset_password">确认修改</iButton>
+				</div>
+			</iForm>
 		</div>
+		<ul class="tab">
+			<li><router-link class="register" :to="{ name: 'login' }">登录</router-link></li>
+			<li><router-link class="register" :to="{ name: 'register' }">注册</router-link></li>
+		</ul>
 	</div>
 </template>
 
