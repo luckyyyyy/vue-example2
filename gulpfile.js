770801c6dc628a69ba86cbb6dac8556dda3e6802
William Chan <root@williamchan.me>
2017-04-01 16:46:57 +0800

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 * @Author: William Chan
 * @Date:	 2017-02-26 13:20:04
 * @Last Modified by:   William Chan
-* @Last Modified time: 2017-03-30 01:01:09
+* @Last Modified time: 2017-04-01 16:46:40
 */
 
 'use strict';
@@ -25,7 +25,7 @@ gulp.task('publish', () =>
 			accessKeyId: 'LTAIucdfDsTvyjjx',
 			secretAccessKey: 'emSGttqwegQcOMxsmEoutPDUVAePfE',
 			endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',
-			bucket: 'rainbow-static'
+			bucket: 'rainbow-global'
 		},
 		headers: {
 			CacheControl: 'max-age=604800',
