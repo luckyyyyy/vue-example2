5e746b6393bd6f11cd6c0ecf36652f0cd546b206
William Chan <root@williamchan.me>
2017-04-09 00:47:27 +0800

    fixed

--- src/views/account/flow.vue
+++ src/views/account/flow.vue
@@ -1,50 +1,46 @@
 <template>
 	<div class="container">
 		<!-- 流量管理 -->
-		<div class="card-wrap">
-			<Card class="border-r" :bordered="false" dis-hover>
-				<p slot="title" class="title">流量管理</p>
-				<p class="content">当前流量余额：<span class="count">¥ 0.00</span></p>
-			</Card>
-		</div>
+		<Card class="commoon-card" :bordered="false" dis-hover>
+			<p slot="title" class="commoon-card__title">流量管理</p>
+			<p class="content">当前流量余额：<span class="count">¥ 0.00</span></p>
+		</Card>
 		<!-- 充值 -->
-		<div class="card-wrap">
-			<Card class="border-r" :bordered="false" dis-hover>
-				<p slot="title" class="title">一元起充</p>
-				<!-- tips -->
-				<div class="content content-info">
-					<Input class="charge" placeholder="请输入充值金额"></Input>
-					<p class="tips">
-						<span>注：起充金额 ≥ 1元；流量单价 0.06 元/人/分钟。</span>
-						<span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp观看费用 = 直播、点播累计观看时长 x 单价。</span>
-						<span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp单人次不满1分钟不计费。</span>
-						<span class="tips-box">有效期：无限期</span>
-						<span class="tips-box">
-							<Checkbox v-model="isAgree"><a href="#">《 彩虹云直播平台服务条款 》</a></Checkbox>
-						</span>
-					</p>
-					<p class="price-box">总价：<span class="price">￥ 0.00</span></p>
-				</div>
-				<!-- 支付方式 -->
-				<div class="content content-info">
-					<Radio-group v-model="pay_method" class="pay-box">
-						<Radio :label="1">
-							<img class="pay-mothod" src="../../assets/images/account/alipay.png" height="43" width="125" alt="支付宝支付">
-						</Radio>
-						<Radio :label="2">
-							<img class="pay-mothod" src="../../assets/images/account/offline.png" height="43" width="125" alt="线下支付">
-						</Radio>
-						<Radio :label="3">
-							<img class="pay-mothod" src="../../assets/images/account/yu'e.png" height="43" width="125" alt="余额支付">
-						</Radio>
-					</Radio-group>
-				</div>
-				<!-- 支付按钮 -->
-				<div class="content">
-					<Button type="error" class="pay-btn">确认支付</Button>
-				</div>
-			</Card>
-		</div>
+		<Card class="commoon-card" :bordered="false" dis-hover>
+			<p slot="title" class="commoon-card__title">一元起充</p>
+			<!-- tips -->
+			<div class="content content-info">
+				<Input class="charge" placeholder="请输入充值金额"></Input>
+				<p class="tips">
+					<span>注：起充金额 ≥ 1元；流量单价 0.06 元/人/分钟。</span>
+					<span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp观看费用 = 直播、点播累计观看时长 x 单价。</span>
+					<span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp单人次不满1分钟不计费。</span>
+					<span class="tips-box">有效期：无限期</span>
+					<span class="tips-box">
+						<Checkbox v-model="isAgree"><a href="#">《 彩虹云直播平台服务条款 》</a></Checkbox>
+					</span>
+				</p>
+				<p class="price-box">总价：<span class="price">￥ 0.00</span></p>
+			</div>
+			<!-- 支付方式 -->
+			<div class="content content-info">
+				<Radio-group v-model="pay_method" class="pay-box">
+					<Radio :label="1">
+						<img class="pay-mothod" src="../../assets/images/account/alipay.png" height="43" width="125" alt="支付宝支付">
+					</Radio>
+					<Radio :label="2">
+						<img class="pay-mothod" src="../../assets/images/account/offline.png" height="43" width="125" alt="线下支付">
+					</Radio>
+					<Radio :label="3">
+						<img class="pay-mothod" src="../../assets/images/account/yu'e.png" height="43" width="125" alt="余额支付">
+					</Radio>
+				</Radio-group>
+			</div>
+			<!-- 支付按钮 -->
+			<div class="content">
+				<Button type="error" class="pay-btn">确认支付</Button>
+			</div>
+		</Card>
 	</div>
 </template>
 
--- src/views/channel/index.vue
+++ src/views/channel/index.vue
@@ -1,61 +1,61 @@
 <template>
-	<div class="container">
-		<div class="card-wrap">
-			<Card class="border-r" :bordered="false" dis-hover>
-				<p slot="title" class="title">频道主页</p>
-				<div class="user">
-					<div class="avatar-box">
-						<img class="avatar" src="../../assets/images/live/share.jpeg" height="50" width="50">
-					</div>
-					<div class="content">
-						<p class="user-name"><span>黑肥麻衣</span>的频道</p>
-						<p class="create-time">创建时间：<span>2017.03.31</span></p>
-					</div>
-					<div class="button-box">
-						<Button class="btn" type="ghost" shape="circle">编辑</Button>
-						<Poptip title="提示标题" content="提示内容" width="200">
-							<Button class="btn" type="ghost" shape="circle">频道地址</Button>
-							<div class="api" slot="content">
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        			</div>
-						</Poptip>
-					</div>
-				</div>
-			</Card>
-		</div>
-		<div class="card-wrap">
-			<Card class="border-r" :bordered="false" dis-hover>
-				<p slot="title" class="title">选择模版</p>
-				<Radio-group class="wrap" v-model="template">
-					<div class="template-box">
-						<div class="template"></div>
-						<div class="settings">
-							<Radio :label="1">
-								<span class="txt">BANNER模版</span>
-							</Radio>
-							<Button @click="selectTemplate(1)" class="btn" type="ghost" shape="circle">
-								编辑
-							</Button>
+	<div>
+		<div class="container">
+			<div class="body">
+				<Card class="commoon-card" :bordered="false" dis-hover>
+					<p slot="title" class="commoon-card__title">频道主页</p>
+					<div class="user">
+						<div class="avatar-box">
+							<img class="avatar" src="../../assets/images/live/share.jpeg" height="50" width="50">
+						</div>
+						<div class="content">
+							<p class="user-name"><span>黑肥麻衣</span>的频道</p>
+							<p class="create-time">创建时间：<span>2017.03.31</span></p>
+						</div>
+						<div class="button-box">
+							<Button class="btn" type="ghost" shape="circle">编辑</Button>
+							<Poptip title="提示标题" content="提示内容" width="200">
+								<Button class="btn" type="ghost" shape="circle">频道地址</Button>
+								<div class="api" slot="content">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		        		</div>
+							</Poptip>
 						</div>
 					</div>
-					<div class="template-box">
-						<div class="template"></div>
-						<div class="settings">
-							<Radio :label="2">
-								<span class="txt">店招模版</span>
-							</Radio>
-							<Button @click="selectTemplate(2)" class="btn" type="ghost" shape="circle">
-								编辑
-							</Button>
+				</Card>
+				<Card class="commoon-card" :bordered="false" dis-hover>
+					<p slot="title" class="commoon-card__title">选择模版</p>
+					<Radio-group class="wrap" v-model="template">
+						<div class="template-box">
+							<div class="template"></div>
+							<div class="settings">
+								<Radio :label="1">
+									<span class="txt">BANNER模版</span>
+								</Radio>
+								<Button @click="selectTemplate(1)" class="btn" type="ghost" shape="circle">
+									编辑
+								</Button>
+							</div>
 						</div>
-					</div>
-				</Radio-group>
-			</Card>
+						<div class="template-box">
+							<div class="template"></div>
+							<div class="settings">
+								<Radio :label="2">
+									<span class="txt">店招模版</span>
+								</Radio>
+								<Button @click="selectTemplate(2)" class="btn" type="ghost" shape="circle">
+									编辑
+								</Button>
+							</div>
+						</div>
+					</Radio-group>
+				</Card>
+			</div>
 		</div>
 	</div>
 </template>
