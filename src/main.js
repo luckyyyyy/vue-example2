cdcf98069badda4daf96e2df5c48f2c57ffb2fe9
储笑 <chuxiao@chuxiaodeMBP.lan>
2017-05-08 18:00:41 +0800

    fixed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import iView from 'iview'
 import ElementUI from 'element-ui'
 import { sync } from 'vuex-router-sync'
 import FastClick from 'fastclick'
@@ -7,8 +6,8 @@ import FastClick from 'fastclick'
 import App from '@/App.vue'
 import { store } from '@/store'
 import router from '@/router'
-import 'animate.css'
-import '@/assets/styles/iview-theme/index.less'
+
+import 'normalize.css'
 import '@/theme/index.css'
 import '@/assets/common.less'
 
@@ -16,7 +15,6 @@ import Icard from '@/components/item/card'
 
 Vue.config.productionTip = false;
 sync(store, router) // done.
-Vue.use(iView);
 Vue.use(ElementUI);
 
 Vue.component('Icard', Icard);
