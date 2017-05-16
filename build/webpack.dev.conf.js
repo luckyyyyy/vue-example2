e1cd60c2943307ae0ff6e42fa6773e981316bd5f
青阳魂 <zjmail.chen@gmail.com>
2017-05-16 14:28:32 +0800

    fixed

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -17,6 +17,9 @@ module.exports = merge(baseWebpackConfig, {
   },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
+  output: {
+    path: config.build.assetsRoot,
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,36 @@
 * @Author: William Chan
 * @Date:	 2017-02-26 13:20:04
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-04-02 01:06:19
+* @Last Modified time: 2017-05-16 14:25:41
 */
 
-'use strict';
+const gulp = require('gulp');
+const ossSync = require('gulp-oss-sync');
 
-var gulp = require('gulp');
-var publish = require('gulp-oss-publish');
+const ossConf = {
+  connect: {
+    "region": "oss-cn-hangzhou",
+    "accessKeyId": "LTAIucdfDsTvyjjx",
+    "accessKeySecret": "emSGttqwegQcOMxsmEoutPDUVAePfE",
+    "bucket": "rainbow-global"
+  },
+  // controls: {
+  //   "headers": {
+  //     "Cache-Control": "no-cache"
+  //   }
+  // },
+  setting: {
+    dir: "b", // root directory name
+    noClean: false, // compare with the last cache file to decide if the file deletion is need
+    force: false, // ignore cache file and force re-upload all the files
+    quiet: true // quiet option for oss deleteMulti operation
+  }
+};
+const cacheConf = {
+  cacheFileName: '.oss-cache-test' // the filename for the cache file
+};
 
-gulp.task('publish', () =>
-	gulp.src('dist/**/*', {
-		base: 'dist',
-		buffer: true
-	}).pipe(publish({
-		prefix: 'b/',
-		genShortId: false,
-		oss: {
-			accessKeyId: 'LTAIucdfDsTvyjjx',
-			secretAccessKey: 'emSGttqwegQcOMxsmEoutPDUVAePfE',
-			endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',
-			bucket: 'rainbow-global'
-		},
-		headers: {
-			CacheControl: 'max-age=604800',
-			ContentEncoding: '',
-		}
-	}))
-);
+gulp.task('publish', function () {
+  return gulp.src('dist/**/*').pipe(ossSync(ossConf, cacheConf));
+})
+
